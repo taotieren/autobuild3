d1ffa8e8c20c87fa2033b17d89e8cf925fab6b5a
Icenowy Zheng <icenowy@outlook.com>
2015-03-23 17:24:59 +0800

    fileenum modification, in order to bishize

--- lib/fileenum.sh
+++ lib/fileenum.sh
@@ -1,16 +1,6 @@
 abreqexe find
 
-fileenum_fromstdin() {
-	while read a; do
-	[ "$a" = ":EXIT" ] && return
-	[ "$a" = "" ] && continue
-	eval `echo $1 | sed "s@{}@$a@g"`
-	done
-}
-
 fileenum() {
-#	(find ; echo :EXIT) | fileenum_fromstdin "$*"
-#A buggy implenmentaion
 	for i in `find .`
 	do
 		[ ! -e $i ] && continue
