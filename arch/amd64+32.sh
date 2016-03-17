d0ca889b7fc8b571624f5c947d2a2cc2b1961226
Mingye Wang (Arthur2e5) <arthur200126@gmail.com>
2016-03-17 15:31:31 -0400

    arch: trying to make #31 clear..
    @MingcongBai Guess what, i686-w64 is just win32 in MinGW64.
    
    https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/rubenvb/gcc-4.7-release/

--- arch/amd64+32.sh
+++ arch/amd64+32.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-##arch/i386.sh: Build definitions for i386 (subsystem only).
+##arch/amd64+32.sh: Build definitions for +32 packages of amd64 arch, i.e. i686 pkgs (optenv only).
 ##@copyright GPL-2.0+
-[[ $ABBUILD != $ABHOST ]] || aberr "The i386 arch is cross-only, for multilib purposes."
 CFLAGS_COMMON_ARCH='-march=pentium4 -mtune=core2 -msse -msse2 -msse3 '
 HOSTTOOLPREFIX=/opt/32/bin/i686-pc-linux-gnu
 export PKG_CONFIG_DIR=/opt/32/lib/pkgconfig
